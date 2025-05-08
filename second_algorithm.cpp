4ef33afddc538cf67a1fa1a177b515107f98e1a3
mahmoud alaa eldin <168581118+mahmoudss5@users.noreply.github.com>
2025-05-08 17:00:35 +0300

    Update second_algorithm.cpp

--- second_algorithm.cpp
+++ second_algorithm.cpp
@@ -1,25 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
+  // recursive algorithm with lamda function  and 2d vectors :
+signed main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> a(n, vector<int>(n));
 
-signed main(){
-    int n; cin>>n;
- int a[n][n];
-    for (int i = 0; i <n ; ++i) {
-        for (int j = 0; j <n ; ++j) {
-            cin>>a[i][j];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> a[i][j];
         }
     }
-    int idx=n-1;
-    int idy=0;
-    int sum1{};
-    int sum2{};
-    for (int i = 0; i <n ; ++i) {
-        sum1+=a[i][idy];
-        idy++;
-        sum2+=a[i][idx];
-        idx--;
-    }
- cout<<abs(sum1-sum2)<<endl;
-return 0;
+
+    int sum1 = 0, sum2 = 0;
+
+    function<void(int, int)> rec = [&](int i, int j) {
+        sum1 += a[i][j];
+        if (i == n - 1 && j == 0) return;
+        rec(i + 1, j - 1);
+    };
+
+    function<void(int, int)> rec2 = [&](int i, int j) {
+        sum2 += a[i][j];
+        if (i == n - 1 && j == n - 1) return;
+        rec2(i + 1, j + 1);
+    };
+
+    rec(0, n - 1);
+    rec2(0, 0);
+
+    cout << abs(sum1 - sum2) << endl;
+
+    return 0;
 }
